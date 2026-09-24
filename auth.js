31564e44aa971dc9c938367287ce0c38f5e8c405
schoolvoice2025-hash <schoolvoice2025@gmail.com>
2026-09-24 23:25:56 +0100

    Delete auth.js

--- auth.js
+++
@@ -1,8 +0,0 @@
-function exigerAuthAdmin(req, res, next) {
-  if (req.session && req.session.estAdmin) {
-    return next();
-  }
-  return res.redirect("/admin/login");
-}
-
-module.exports = { exigerAuthAdmin };
